b37eaae264abd5abb67c2a43b77aa27f94f5ec6e
manny404 <manny@art404.com>
2016-03-28 22:19:12 -0400

    make it so

---
+++ README.md
@@ -0,0 +1,40 @@
+# for-every-fav
+
+An app that uses [Twitter user streams](https://dev.twitter.com/streaming/userstreams) to listen to favorites on a specific tweet, and then trigger an action.
+Currently it is deleting one of [my](http://twitter.com/artnotfound) tweets randomly.
+
+## Usage
+
+If you want to use it yourself, set up a `config.json` that looks like:
+
+```js
+{
+  "user": {
+    "name": "your_username", // eg "SHAQ"
+    "id": your_userid, // eg 17461978
+    "tweet_id_str": "id_str_of_tweet_to_track" //eg "713479668221906944"
+  },
+  "api": {
+    "consumer_key": "XXXX",
+    "consumer_secret": "XXX",
+    "access_token_key": "XXX",
+    "access_token_secret": "XXX"
+  }
+}
+
+```
+
+and an `ALL.json` file with all your tweets in the format:
+
+[
+  {
+    "id_str": "6723587233",
+    "text": "If u feel alone and by yourself, look in the mirror, and wow, there's two of you.  Be who you are. Who are you. I am me. Ugly, lol.   Shaq"
+  },
+  {
+    "id_str": "3576781019",
+    "text": "went to the la zoo today, a chimpanzee spit at me, dam i must be ugly, lol"
+  }
+]
+
+then run `node app`
---
+++ app.js
@@ -0,0 +1,69 @@
+var config = require('./config.json');
+var Twitter = require('twitter');
+var Stream = require('user-stream');
+var colors = require('colors');
+var low = require('lowdb');
+var _ = require('lodash');
+var storage = require('lowdb/file-sync');
+
+var state = low('./state.json', {'storage': storage});
+var stream = new Stream(config.api);
+var client = new Twitter(config.api);
+
+if (!state.object.tweets.length && !state.object.tweets_deleted.length) {
+  state.object.tweets = require('./tweets/ALL');
+  state.object.tweets = state.object.tweets.filter(function(t) {
+    return t.id_str !== config.user.tweet_id_str;
+  });
+  state.write();
+}
+
+stream.stream();
+
+stream.on('data', function(json) {
+  if (!json.event) return;
+
+  var isFav = json.event === 'favorite';
+  var isTweet = json.target_object.id_str === config.user.tweet_id_str;
+
+  if (isFav && isTweet) {
+    deleteRandomTweet(json.source.screen_name);
+  }
+});
+
+function deleteRandomTweet(user) {
+  var alreadySubmitted = state.object.users_submitted;
+  if (alreadySubmitted.indexOf(user) > -1) {
+    console.log('');
+    console.log(user.yellow.bold, ' TRIED TO SUBMIT AGAIN!'.red.bold);
+    return;
+  }
+
+  var nextTweet = _.sample(state.object.tweets);
+
+  client.post('statuses/destroy', {id: nextTweet.id_str}, function(err, resp) {
+    if (err) {
+      console.log('');
+      console.log('TWEET ERRORED !!!'.bold.red);
+      console.log(JSON.stringify(err, 0, 2).red);
+    } else {
+      state.object.users_submitted.push(user);
+      state.object.tweets_deleted.push(nextTweet);
+      state.object.tweets = _.without(state.object.tweets, nextTweet);
+      state.write();
+      console.log('');
+      console.log('~~~~~~~~~~~~~~~~'.rainbow);
+      console.log('');
+      console.log('USER '.cyan, user.yellow.bold, ' DELETED:'.cyan);
+      console.log(nextTweet.text.white);
+      console.log('');
+      console.log('~~~~~~~~~~~~~~~~'.rainbow);
+    }
+  });
+}
+
+stream.on('error', function(err) {
+  console.log('');
+  console.log('STREAM ERRORED !!!'.bold.red);
+  console.log(JSON.stringify(err, 0, 2).red);
+});
